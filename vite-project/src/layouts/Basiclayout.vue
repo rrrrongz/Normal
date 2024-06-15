5856b914a5856e212f4ba95429780ce6869fdf0d
rong <2944345603@qq.com>
2024-06-16 04:23:02 +0800

    导航栏

---
+++ vite-project/src/layouts/Basiclayout.vue
@@ -0,0 +1,33 @@
+<template>
+  <van-nav-bar
+      title="标题"
+      fixed
+      left-arrow
+      @click-left="onClickLeft"
+      @click-right="onClickRight"
+  >
+    <template #right>
+      <van-icon name="search" size="18" />
+    </template>
+  </van-nav-bar>
+
+  <van-tabbar v-model="active" @change="onChange">
+    <van-tabbar-item icon="home-o" name="index">主页</van-tabbar-item>
+    <van-tabbar-item icon="search" name="team">队伍</van-tabbar-item>
+    <van-tabbar-item icon="friends-o" name="user">个人</van-tabbar-item>
+
+  </van-tabbar>
+
+</template>
+
+<script setup lang="ts">
+import {showToast} from "vant";
+import {ref} from "vue";
+
+const onClickLeft = () => history.back();
+const onClickRight = () => showToast('按钮');
+
+const active = ref(0);
+const onChange = (index) => showToast(`标签 ${index}`);
+</script>
+
--- vite-project/src/main.ts
+++ vite-project/src/main.ts
@@ -2,11 +2,14 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-import { Button, NavBar} from 'vant';
+import { Button, NavBar,Icon,Tabbar, TabbarItem} from 'vant';
 import 'vant/lib/index.css';
 
 const app = createApp(App)
 app.use(Button);
 app.use( NavBar);
+app.use(Icon);
+app.use(Tabbar)
+app.use(TabbarItem)
 
 app.mount('#app')
